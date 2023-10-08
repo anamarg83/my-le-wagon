b04d92dd369180c03580f2ba25a9693b6f5a83b2
Margarida Ferreira <margarida.ferreira@xing.com>
2023-10-08 15:56:04 +0100

    removed some comments to the demo file

--- lewagon/data/00-Setup/check/demo.py
+++ lewagon/data/00-Setup/check/demo.py
@@ -4,7 +4,6 @@ import math
 
 def circle_area(radius):
     """Returns the area of the circle of given radius"""
-    # area = math.pi * radius * radius 
     if(radius > 0):
         return math.pi * radius * radius 
     return 0
