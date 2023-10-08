d465edae07556365900ddbcd8e6fe812a9907c1e
Margarida Ferreira <margarida.ferreira@xing.com>
2023-10-08 16:08:03 +0100

    Fix style issues, should get a 'Good Style' now 🙏

--- lewagon/data/00-Setup/git/today.py
+++ lewagon/data/00-Setup/git/today.py
@@ -1,9 +1,15 @@
 '''
-This is the module docstring
+A module computing buddy pair names for the day
 '''
 
-def my_name_is(): 
+def my_name_is():
+    '''
+    my_name_is function returns my nickname in github page
+    '''
     return "anamarg83"
 
-def my_buddy_is(): 
+def my_buddy_is():
+    '''
+    my_buddy_is function returns my buddy's nickname in git
+    '''
     return "amargaridaferreira"
